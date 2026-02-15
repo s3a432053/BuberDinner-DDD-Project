37e684ef1a8c39c6e251b4fc17a65251742a4e05
HCHSU19 <HCHSU19@winbond.com>
2026-02-15 21:07:08 +0800

    feat: 新增 AggregateRoot 和 Entity 抽象類別實作

---
+++ BuberDinner.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,10 @@
+namespace BuberDinner.Domain.Common.Models
+{
+    public class AggregateRoot<TId> : Entity<TId>
+    where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id)
+        {
+        }
+    }
+}
